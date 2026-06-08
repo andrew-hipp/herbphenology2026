b6edb75c983be8a645a854467e1a195ddf2ffc59
Andrew Hipp <andrew-hipp@users.noreply.github.com>
2026-06-08 11:20:36 -0500

    Update script

--- scripts/00.readData.R
+++ scripts/00.readData.R
@@ -1,2 +1,7 @@
 library(openxlsx)
 dat.gr <- read.xlsx('data/Herbarium_GREEN_Specimens.xlsx')
+
+dat.gr.summary <- 
+    table(dat.gr$CalcFullName) |> 
+    sort(decreasing = T)
+write.csv(dat.gr.summary, 'out/dat.gr.summary.csv')
