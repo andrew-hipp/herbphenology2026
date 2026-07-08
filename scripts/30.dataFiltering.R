1e8f149552d54d934f895ec0909de9a95cda02a7
Andrew Hipp <andrew-hipp@users.noreply.github.com>
2026-07-08 10:27:50 -0500

    subsetting basics fixed

--- scripts/30.dataFiltering.R
+++ scripts/30.dataFiltering.R
@@ -4,6 +4,24 @@
 
 if(!exists('dat.mat')) stop('** Run scripts 00 and 10 before this one **')
 
-dat.sum.byTaxname <- dat.mat$spClean |>
-  table() |>
-  sort(decreasing = T)
+dat_ph <- list(
+    ph1.3 = dat.mat[dat.mat$Phenophase %in% 1:3, ],
+    ph4.6 = dat.mat[dat.mat$Phenophase %in% 4:6, ],
+    ph7.8 = dat.mat[dat.mat$Phenophase %in% 7:8, ]
+)
+
+dat.sum <- sapply(dat_ph, function(x, hybexclude = T, cultexclude = T) {
+    if(hybexclude) x <- x[!x$hyb, ]
+    if(cultexclude) x <- x[!x$cult, ]
+    x$spClean |>
+    table() |>
+    sort(decreasing = T)
+    
+}
+)
+
+whosin <- function(x, thresh = 5) {
+    names(which(x >= thresh))
+}
+
+keepsies <- sapply(dat.sum, whosin)
