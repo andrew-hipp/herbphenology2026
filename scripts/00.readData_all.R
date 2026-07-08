ed52dbdfbd7c260f4a99d7fc90dec092b89eba7e
Andrew Hipp <andrew-hipp@users.noreply.github.com>
2026-07-08 08:54:47 -0500

    concatenating matrices

--- scripts/00.readData_all.R
+++ scripts/00.readData_all.R
@@ -2,13 +2,24 @@
 
 require(openxlsx)
 
+## summarize what taxa we have in the green specimens
 dat.gr <- read.xlsx('data/Herbarium_GREEN_Specimens.xlsx')
 dat.gr.summary <- 
     table(dat.gr$CalcFullName) |> 
     sort(decreasing = T)
 write.csv(dat.gr.summary, 'out/dat.gr.summary.csv')
 
-dat.til <- 
-dat.tilam <- 
-dat.cer <- 
-dat.acer <- 
+dat.list <- list(
+    dat.til = read.xlsx('data/Tilia_everything_else.xlsx'),
+    dat.tilam = read.xlsx('data/Tilia_americana.xlsx'),
+    dat.cer = read.xlsx('data/Cercis.xlsx'),
+    dat.acer = read.xlsx('data/Acer.xlsx')
+)
+
+## check columns just to be sure
+temp <- sapply(dat.list, names)
+if(!Reduce(identical,temp)) warning('** data headers do not all match... beware!! **')
+colsToUse <- Reduce(intersect, temp)
+
+dat.list <- lapply(dat.list, function(x) x[,colsToUse])
+dat.mat <- Reduce(rbind, dat.list)
